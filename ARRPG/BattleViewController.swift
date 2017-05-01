1dfbe20c94ad36841753ea4723117daba54571c7
Fauxpaw <fauxpawsc@live.com>
2017-05-01 02:21:42 -0700

    add camera session to battle

--- ARRPG/BattleViewController.swift
+++ ARRPG/BattleViewController.swift
@@ -14,18 +14,75 @@ import AVFoundation
 class BattleViewController: UIViewController {
 
     let player = Character(hp: 100, mp: 10)
+    var cameraSession: AVCaptureSession?
+    var cameraLayer: AVCaptureVideoPreviewLayer?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
-        
+        self.loadCamera()
+        self.cameraSession?.startRunning()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     
     
+    }
+    
+    func createCaptureSession() -> (session: AVCaptureSession?, error: NSError?) {
+        
+        var error: NSError?
+        var captureSession: AVCaptureSession?
+        
+        let backVideoDevice = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: .back)
+        
+        if backVideoDevice != nil {
+            var videoInput : AVCaptureDeviceInput!
+            do {
+                videoInput = try AVCaptureDeviceInput(device: backVideoDevice)
+            } catch let error1 as NSError {
+                error = error1
+                videoInput = nil
+            }
+            
+            if error == nil {
+                captureSession = AVCaptureSession()
+                
+                if captureSession!.canAddInput(videoInput) {
+                    captureSession?.addInput(videoInput)
+                } else {
+                    error = NSError(domain: "", code: 0, userInfo: ["description": "Error adding video input"])
+                }
+            } else {
+                error = NSError(domain: "", code: 1, userInfo: ["description": "Error creating capture device input"])
+            }
+            
+        } else {
+            error = NSError(domain: "", code: 2, userInfo: ["description":"back video device not found"])
+        }
+        
+        return (session: captureSession, error: error)
+        
+    }
+    
+    func loadCamera() {
+        let captureSessionResult = self.createCaptureSession()
+        
+        guard captureSessionResult.error == nil, let session = captureSessionResult.session else {
+            print("error creating capture session")
+            return
+        }
+        
+        self.cameraSession = session
+        
+        if let cameraLayer = AVCaptureVideoPreviewLayer(session: self.cameraSession) {
+            cameraLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+            cameraLayer.frame = self.view.bounds
+            self.view.layer.insertSublayer(cameraLayer, at: 0)
+            self.cameraLayer = cameraLayer
+        }
+        
     }
  
     @IBAction func attackButtonPressed(_ sender: Any) {
