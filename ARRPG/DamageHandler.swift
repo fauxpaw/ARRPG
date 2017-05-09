3bdf05c7c518952e4fc5082093c4d5321febb2e2
Fauxpaw <fauxpawsc@live.com>
2017-05-08 18:45:44 -0700

    update dmg algorithm to reflect new stats

--- ARRPG/DamageHandler.swift
+++ ARRPG/DamageHandler.swift
@@ -14,26 +14,26 @@ class DamageHandler {
     //elemental dmg
     //base def
     //elemental res
+    
     static let shared = DamageHandler()
     
     private init() {}
     
-    //Physical DMGs
     func calculateBaseDMG(attacker: Entity, defender: Entity) -> Int {
         // atk * atk / (atk + def)
-        let dmg = attacker.atk.getValue() * attacker.atk.getValue() / (attacker.atk.getValue() + defender.def.getValue())
-        return dmg
-    }
-    
-    func calculatePiercingDMG(attacker: Entity, defender: Entity) -> Int {
-        // atk * atk / (atk + def)
-        let dmg = attacker.atk.getValue() * attacker.atk.getValue() / (attacker.atk.getValue() + defender.def.getValue())
-        return dmg
-    }
-    
-    //Magical DMGs
-    func calculateDMG(attacker: Entity, defender: [Entity]) {
+        let attack = [attacker.atk.getValue(), attacker.PAT.getValue(), attacker.CAT.getValue(), attacker.SAT.getValue(), attacker.MAT.getValue(), attacker.AAT.getValue(), attacker.FAT.getValue(), attacker.WAT.getValue(), attacker.EAT.getValue()]
+        let defense = [defender.def.getValue(), defender.PDF.getValue(), defender.CDF.getValue(), defender.SDF.getValue(), defender.MDF.getValue(), defender.AR.getValue(), defender.FR.getValue(), defender.WR.getValue(), defender.ER.getValue()]
+        print("attackArray: \(attack)")
+        print("defenseArray: \(defense)")
+        var dmg = 0
         
+        for (index, num) in attack.enumerated() {
+            if num > 0 || defense[index] > 0 {
+                dmg += Int(attack[index] * attack[index] / (attack[index] + defense[index]))
+            }
+        }
+        
+        return dmg
     }
     
     func heal(caster: Entity, target: Entity) -> Int {
